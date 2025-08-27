d3d3b612e5af7c772dbdf1c6ab193845974ef110
jbanes <jbanes@gmail.com>
2025-08-26 23:26:07 -0500

    Converted Character to streaming format

---
+++ src/main/webapp/js/chat.js
@@ -0,0 +1,60 @@
+async function processResponse(response, output)
+{
+    var reader = response.body.getReader();
+    var complete = false;
+    var text = "";
+    
+    output.scrollTo(0, 0);
+
+    while(!complete)
+    {
+        const { value, done } = await reader.read();
+        const lines = new TextDecoder().decode(value).split(/\r?\n|\r/);
+
+        for(var line of lines)
+        {
+            if(line.trim().length < 1) continue;
+
+            try
+            {
+                line = JSON.parse(line);
+            }
+            catch(e)
+            {
+                console.log("Failed line:", line);
+                console.error(e);
+            }
+
+            if(line.action === 'generateReport')
+            {
+                generateReport(line);
+                continue;
+            }
+
+            text += line.message ? line.message.content : line.response;
+            output.innerHTML = marked.parse(text);
+
+            if(line.done) break;
+        }
+
+        complete = done;
+    }
+}
+
+async function chat(url, question, output, callback)
+{
+    try
+    {
+        var response = await fetch(url + "?chat=" + encodeURIComponent(question));
+        
+        if(callback) callback(response.ok);
+
+        await processResponse(response, output);
+    }
+    catch(e)
+    {
+        output.innerHTML = marked.parse("**I'm sorry!** I think I misunderstood and am unable to process your request. Can you rephrase your request?");
+
+        console.error(e);
+    }
+}
